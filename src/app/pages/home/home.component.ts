71d6a15f6ddcaf95d37ae733411208340fb36f96
Justin Johnson <justin.johnson@SB-FL-K4XCGTFM.local>
2018-01-12 21:35:21 -0500

    Updated home page menu layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
 
+  private navigate(page): void{
+    this.router.navigate([`/${page}`]);
   }
+
 
 }
