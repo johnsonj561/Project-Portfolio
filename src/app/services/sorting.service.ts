e8ec2fb921a2416cd464d88fa9609037a7614585
Justin Johnson <justin.johnson@SB-FL-K4XCGTFM.local>
2018-01-20 22:09:40 -0500

    Fixing category sort

--- src/app/services/sorting.service.ts
+++ src/app/services/sorting.service.ts
@@ -12,7 +12,7 @@ export class SortingService {
   private categoryCompareMethods: any = {
     "alphabetical": (a, b) => {
       const _a = a.name.toLowerCase(),
-      _b = b.name.toLowerCase;
+      _b = b.name.toLowerCase();
       if(_a > _b) return 1;
       else return (_a < _b) ? -1 : 0;
     },
@@ -24,7 +24,7 @@ export class SortingService {
         return (a.name.toLowerCase() > b.name.toLowerCase()) ? -1 : 1;
       }
       // else return the one that has 0 instances
-      else return (a.instances) ? 1 : -1;
+      else return (a.instances) ? -1 : 1;
     }
   }
 
